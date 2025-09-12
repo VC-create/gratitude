c98f27543c5cfbadab8115f348bfe68a74d1b5fd
VC-create <vedika.ar.chaudhari@gmail.com>
2025-09-12 01:07:03 +0000

    added some comments explaining the js, got rid of old code

--- pages/static/script.js
+++ pages/static/script.js
@@ -1,36 +1,7 @@
 function toggleTheme(theme) {
-
-            // Obtain the name of stylesheet 
-            // as a parameter and set it 
-            // using href attribute.
-            let themeLink = document
-                .getElementById('theme');
-
-            themeLink.href = "/static/" + theme
-        }
-
-
-
-//switches = document.getElementsByClassName("switch");
-
-//loops through switches
-//for(let i of switches){
-    //this adds event listener to each of the switches
-    //i.addEventListener("click", () => {
-        //let theme = i.dataset.theme;
-        //setTheme(theme)
-    //})
-//}
-
-//function setTheme(theme){
-    //switch(theme){
-        //case "summer": document.getElementById("switcher-id").href = "/static/summer.css";
-        //return;
-        //case "fall": document.getElementById("switcher-id").href = ".static/fall.css"
-        //return;
-        //case "winter": document.getElementById("switcher-id").href = ".static/winter.css"
-        //return;
-        //case "spring": document.getElementById("switcher-id").href = ".static/spring.css"
-        //return;
-    //}
-//}
+    //get the link to the sylesheet using its id
+    let themeLink = document.getElementById('theme');
+    //update the link to the stylesheet by having the href link go into the static folder and then find the theme
+    //the theme is passed when we call the function in base.html
+    themeLink.href = "/static/" + theme
+}
