3d88233a339347dbe6e62b8f8bdbeec8ff60bb58
VC-create <vedika.ar.chaudhari@gmail.com>
2025-09-12 01:02:49 +0000

    linked the multiple stylesheets!

--- pages/static/script.js
+++ pages/static/script.js
@@ -1,23 +1,36 @@
-switches = document.getElementsByClassName("swtich");
+function toggleTheme(theme) {
+
+            // Obtain the name of stylesheet 
+            // as a parameter and set it 
+            // using href attribute.
+            let themeLink = document
+                .getElementById('theme');
+
+            themeLink.href = "/static/" + theme
+        }
+
+
+
+//switches = document.getElementsByClassName("switch");
 
 //loops through switches
-for(i of switches){
+//for(let i of switches){
     //this adds event listener to each of the switches
-    i.addEventListener("click", () => {
-        theme = i.dataset.theme;
-        setTheme(theme)
-    })
-}
+    //i.addEventListener("click", () => {
+        //let theme = i.dataset.theme;
+        //setTheme(theme)
+    //})
+//}
 
-function setTheme(theme){
-    switch(theme){
-        case "summer": document.getElementById("switcher-id").href = "/static/summer.css"
-        return;
-        case "fall": document.getElementById("switcher-id").href = ".static/fall.css"
-        return;
-        case "winter": document.getElementById("switcher-id").href = ".static/winter.css"
-        return;
-        case "spring": document.getElementById("switcher-id").href = ".static/spring.css"
-        return;
-    }
-}
+//function setTheme(theme){
+    //switch(theme){
+        //case "summer": document.getElementById("switcher-id").href = "/static/summer.css";
+        //return;
+        //case "fall": document.getElementById("switcher-id").href = ".static/fall.css"
+        //return;
+        //case "winter": document.getElementById("switcher-id").href = ".static/winter.css"
+        //return;
+        //case "spring": document.getElementById("switcher-id").href = ".static/spring.css"
+        //return;
+    //}
+//}
